727efa5b88639d6975837b9edb19ae389cafebd5
Ana Souza <aninhasouza@gmail.com>
2020-06-12 00:17:24 -0300

    Fixed test1 >> cloned the array to avoid both points to the same reference.

--- src/test1.java
+++ src/test1.java
@@ -1,7 +1,5 @@
-// INPUT[1, 0, 0, 0,0, 1, 0, 0], 1
-// OUTPUT[0 1 0 0 1 0 1 0]
-package tests;
-
+// INPUT [1, 0, 0, 0, 0, 1, 0, 0], 1
+// OUTPUT[0  1  0  0  1  0  1  0]
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,39 +9,44 @@ import java.util.Arrays;
 public class test1 {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-
 		java.io.Reader input = new FileReader("test1.in");
 		// java.io.Reader input = new InputStreamReader(System.in);
-		
 		BufferedReader reader = new BufferedReader(input);
-		
 		String line = reader.readLine();
 		
-		String[] values = line.replace("[", "").replace("]", "").replace(" ", "").split(",");
+		String[] data = line.replace("[", "").replace("]", "").replace(" ", "").split(",");
 		
-		int iterations = Integer.parseInt(values[8]);
+		int days = Integer.parseInt(data[8]);
 		
-		while (iterations-- > 0) {
+		int[] status = new int[8];
+		
+		for (int i = 0; i < 8; ++i) {
+			status[i] = Integer.parseInt(data[i]);
+		}
+		
+		System.out.println(Arrays.toString(status));
+		
+		while (days-- > 0) {
 			
-			String[] auxValues = values;
-			
-			values[0] = (auxValues[1].equals("0"))?"0":"1";
-			
-			for (int j = 1; j < 6; j++ ) {
-				
-				if (auxValues[j-1].equals(auxValues[j+1])) {
-					values[j] = "0";
+			int[] auxStatus = status.clone();
+
+			System.out.println(auxStatus[1]);
+			status[0] = (auxStatus[1]==0) ? 0 : 1;
+			System.out.println(auxStatus[1]);
+			for (int j = 1; j < 7; j++ ) {
+				System.out.println("auxStatus[j-1]==auxStatus[j+1]" + auxStatus[j-1] + " "  + auxStatus[j+1]);
+				if (auxStatus[j-1]==auxStatus[j+1]) {
+					status[j] = 0;
 				} else {
-					values[j] = "1";
+					status[j] = 1;
 				}
 			} 
 
-			values[7] = (auxValues[6].equals("0"))?"0":"1";
+			status[7] = (auxStatus[7]==0) ? 0 : 1;
 			
 		}
 		
-		System.out.println(Arrays.toString(values));
+		System.out.println(Arrays.toString(status));
 	}
 
 }
--- src/test2.java
+++ src/test2.java
@@ -1,5 +1,3 @@
-package tests;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
